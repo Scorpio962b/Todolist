cc6053aa9fc448dd067e430630333025dc65e928
Scorpio962b <samaaiab962@gmail.com>
2023-06-02 16:07:24 +0200

    first

---
+++ script/index.js
@@ -0,0 +1,14 @@
+let toDo[]
+
+function addToDo(){
+    let input = document.getElementById('#myInput').value;
+    let listings = document.getElementById('#listings').value;
+}
+myInput.push(toDo);
+console.log(toDo)
+
+
+
+function sorting(){
+    toDo.sort,
+};
