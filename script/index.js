6d841502de03e0b2d89f1ead0ef4e8bb9d7cfb20
Scorpio962b <samaaiab962@gmail.com>
2023-06-03 14:38:36 +0200

    edits

--- script/index.js
+++ script/index.js
@@ -1,19 +1,31 @@
 let toDo = [];
-let input = document.getElementById('myInput');
-  
+
+function undo(){
+    toDo.pop()
+    console.log(toDo)
+}
+
+console.log(toDo)
 
 function addToDo (){
-    let person = {
+  
+    let item = {
         Id: toDo.indexOf() ,
-        name: document.getElementById('myInput').value,
+        name: document.getElementById("myInput").value,
         createdDate: new Date(),
     };
-    toDo.push(input);
+    toDo.push(item);
     console.log(toDo);
 }
 
 function sorting() {
-    toDo.sort;
+    let sortArray = toDo.sort().asc.describe('first', () => { second })
+     sortArray.forEach(item => {
+        if (item === style.line("line-through"));
+        
+        else {
+            return sortArray;
+        }
+     });
 }
-
 let listings = document.getElementById('listings').innerHTML;
